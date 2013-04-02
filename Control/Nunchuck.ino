4a0ee2c7eaa641c4fe2c63bbefd617c3b0b5133d
mmyers <myers.mm89@gmail.com>
2013-04-02 14:17:20 -0400

    Reorganizing

--- Control/Nunchuck.ino
+++ Control/Nunchuck.ino
@@ -1,33 +1,6 @@
-/*
-*WiichuckSerial
-*
-*Uses Nunchuck Lib from 16.5
-*sends comma-separated values for data
-*Label string separated by commas can be used by receiving program
-*to id fields
-*/
-
-#include <Wire.h>
-#include "Nunchuck.h"
-
 //value to add to the sensor to get zero reading when centered
 int offsetX, offsetY, offsetZ;
 
-void setup()
-{
-  Serial.begin(57600);
-  // ----- Set up Nunchuck -----
-  //nunchuckSetPowerPins();
-  nunchuckInit(); //send the init handshake
-  nunchuckRead(); //ignore first tiem
-
-  delay(50);
-}
-
-void loop()
-{
-  readNunchuck();
-}
 void readNunchuck(){
 
   nunchuckRead();
@@ -50,16 +23,8 @@ void readNunchuck(){
   printButton(zButton);
   int cButton = nunchuckGetValue(wii_btnC); 
   printButton(cButton);
-  Serial.println();
-  
-  
+  Serial.println(); 
 }
-
-
-
-
-
-
 
 //------------------------------------------------------------
 //------------------ Printing nunchuck data ------------------
