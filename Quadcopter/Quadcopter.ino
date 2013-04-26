4eaab11e4dff473384bcdae2b91d611600ab7b1c
mmyers <myers.mm89@gmail.com>
2013-04-26 13:18:37 -0400

    Changes to xbee parse code

--- Quadcopter/Quadcopter.ino
+++ Quadcopter/Quadcopter.ino
@@ -5,14 +5,14 @@
 
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
-//#include "I2Cdev.h"
-//#include "MPU6050.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
 
 // class default I2C address is 0x68
 // specific I2C addresses may be passed as a parameter here
 // AD0 low = 0x68 (default for InvenSense evaluation board)
 // AD0 high = 0x69
-//MPU6050 accelgyro;
+MPU6050 accelgyro;
 
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
@@ -27,15 +27,17 @@ int nunchuckData[5];
 
 void setup()
 {
-  lcd.begin(numCols, numRows);
+  lcd.begin(16, 2);
+  //lcd.setCursor(5,0);
+  lcd.autoscroll();
   //lcd.scrollDisplayLeft();
   Serial.begin(9600);     // 9600 for xBee
   
  // accelgyro.initialize(); // Initialize gyro/accel
   
-  /*Serial.println("Testing device connections...");
-  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-  */
+ /* Serial.println("Testing device connections...");
+  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");*/
+  
   
 }
 
@@ -47,10 +49,8 @@ void loop()
   /* Get data from accelerometer and gyro https://github.com/jrowberg/i2cdevlib
     https://www.sparkfun.com/products/11028 */
     
- // getMotion();
-  
-  
-  
+    //getMotion();
+
   // Adjust motor speeds 
 
   
--- Quadcopter/ReceiveData.ino
+++ Quadcopter/ReceiveData.ino
@@ -6,39 +6,65 @@ String yPlace, zPlace, zbPlace, cPlace;
 // Variables for nunchuck values
 int xVal, yVal, zVal, zBval, cBval;
 
-char inData[40];
+String inData[5];
 byte index = 0;
 int input = 0;
-
+int slow = 0;
 
 
 /* Get acceleration and gyro */
 /*void getMotion(){
-     // read raw accel/gyro measurements from device
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-}*/
-
+ // read raw accel/gyro measurements from device
+ accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+ }*/
+void clear(){
+   for(int i=0;i<5;i++){
+      inData[i] = "";
+   } 
+}
 /* Parses data from Xbee (Nunchuck values) */
 void parseXbeeData(){
-  
+
   int varCt = 0;
   st = "";
   while(Serial.available()){
-      
-      input = Serial.read();
-      if(input != -1){
-        char aChar = char(input);
-      if(aChar == '\n'){ // end of the input line
-        st = st + aChar;
-        /*index = 0;
-        varCt = 0;
-        /* for (int i=0;i<40;i++){
-            Serial.print(inData[i]); 
-            inData[i] = 0;
-         }
-        
-         lcd.setCursor(0,0);
-         lcd.print(st); */
+    
+    char input = char(Serial.read());
+     if(input == '\n'){
+         st = "";
+         //parse();
+         clear();
+         index = 0;
+     }
+     else if (input == ' '){
+       inData[index] = st;
+       lcd.print(st);
+       st = "";
+       index++;
+     }
+     else{
+       st+=input;
+     }
+
+     slow++;
+  }
+}
+
+void parse() {
+   // 2 X 16
+   lcd.setCursor(0,0);
+   lcd.print("x "+inData[0]);
+   lcd.setCursor(5,0);
+   lcd.print("y "+inData[1]);
+   lcd.setCursor(10,0);
+   lcd.print("z "+inData[2]);
+   lcd.setCursor(0,1);
+   lcd.print("zb "+inData[3]);
+   lcd.setCursor(5,1);
+   lcd.print("cb "+inData[4]);
+ 
+ } 
+/*
         Serial.println("About to be parsed"); 
         if(st.indexOf("n") != 1) {
           Serial.println("Parsing has begun");
@@ -48,7 +74,6 @@ void parseXbeeData(){
             lcd.setCursor(0,0);
             lcd.print("x");
             lcd.print(xAccel);
-            
           }
           if(yPlace.indexOf(" ") > 0){
             yAccel = st.substring(0, (st.indexOf(" ")));
@@ -56,15 +81,13 @@ void parseXbeeData(){
             lcd.setCursor(5,0);
             lcd.print("y");
             lcd.print(yAccel);
-            
           }
           if(zPlace.indexOf(" ") > 0){
             zAccel = st.substring(0, (st.indexOf(" ")));
             zbPlace = zPlace.substring((zPlace.indexOf(" "))+1);
-           lcd.setCursor(10,0);
+            lcd.setCursor(10,0);
             lcd.print("z");
             lcd.print(zAccel);
-            
           }
           if(zbPlace.indexOf(" ") > 0){
             zButton = st.substring(0, (st.indexOf(" ")));
@@ -72,67 +95,17 @@ void parseXbeeData(){
             lcd.setCursor(0,1);
             lcd.print("zb");
             lcd.print(zButton);
-            
           }
-         if(cPlace.indexOf(" ") > 0){
-           cButton = st.substring(0, (st.indexOf(" ")));
-           st = cPlace.substring((cPlace.indexOf(" ")) +1);
+          if(cPlace.indexOf(" ") > 0){
+            cButton = st.substring(0, (st.indexOf(" ")));
+            st = cPlace.substring((cPlace.indexOf(" ")) +1);
             lcd.setCursor(7,1);
             lcd.print("cb");
             lcd.print(cButton);
-            
-         
-      }Serial.println("parsed");
-       delay(1000);
-       lcd.clear();
-       }
-      
-      
-      
-      
-     
-      
-      }
-      
-      else{
-        /* inData[index] = aChar;
-         index++;
-         inData[index] = ':'; // Keep the string NULL terminated*/ 
-         st = st + aChar;
-      }
-     
-      
-   }
-}
-
-}
-  
-      
-  
-  
+          }
+          Serial.println("parsed");
+          delay(1000);
+          lcd.clear();
+        }*/
 
 
-/*void parse(int varCt) {
-  int index = 0;
-  String num = "";
-  while(inData[index] != ':'){
-      num = num + inData[index];
-      
-  }
-  nunchuckData[varCt] = num.toInt();
-  // 2 X 16
-  lcd.setCursor(0,0);
-  lcd.print("x "+nunchuckData[0]);
-  lcd.setCursor(5,0);
-  lcd.print("y "+nunchuckData[1]);
-  lcd.setCursor(10,0);
-  lcd.print("z "+nunchuckData[2]);
-  lcd.setCursor(0,1);
-  lcd.print("zb "+nunchuckData[3]);
-  lcd.setCursor(5,1);
-  lcd.print("cb "+nunchuckData[4]);
-  
-} */
-  
-      
-      
